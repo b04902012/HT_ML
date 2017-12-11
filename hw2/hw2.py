f90f16e3725bc200331f7bbbb554a80eef4179e3
b04902012 <b04902012@ntu.edu.tw>
2017-12-11 02:18:42 +0000

    figure

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -9,36 +9,49 @@ def sign(x):
         return 1;
     return -1;
 
-def error(x,y,s,t):
+def error_in(x,y,s,t):
     err=0
     for i in range(t-1,20):
-        if(sign(x[i])!=s):
+        if(y[i]!=s):
             err=err+1
     for i in range(0,t-1):
-        if(sign(x[i])==s):
+        if(y[i]==s):
             err=err+1
-    return err
+    return err/20
+
+def error_out(x,y,s,t):
+    theta=0
+    if(t==0):
+        theta=(-1+x[0])/2
+    elif(t==20):
+        theta=(x[19]+1)/2
+    else:
+        theta=(x[t-1]+x[t])/2
+    return 0.5+0.3*s*(theta*sign(theta)-1)
 
 def train(x,y):
     s=0
     t=0
     err=20
     for ti in range(0,21):
-        if(error(x,y,1,ti)<err):
-            err=error(x,y,1,ti)
+        if(error_in(x,y,1,ti)<err):
+            err=error_in(x,y,1,ti)
             s=1
             t=ti
-        if(error(x,y,-1,ti)<err):
-            err=error(x,y,-1,ti)
+        if(error_in(x,y,-1,ti)<err):
+            err=error_in(x,y,-1,ti)
             s=-1
             t=ti
+
     return [s,t]
     
 
-random.seed(40)
+random.seed(0)
 x = np.zeros(20)
 y = np.zeros(20)
 array=np.zeros(21)
+e_in=np.zeros(1000)
+e_out=np.zeros(1000)
 for a in range(0,1000):
     for i in range(0,20):
         x[i]=random.uniform(-1,1)
@@ -52,7 +65,10 @@ for a in range(0,1000):
     result=train(x,y)
     s=result[0]   
     t=result[1]
-    array[t]=array[t]+1
-for i in range(0,21):
-    print(array[i])
+    e_in[a]=error_in(x,y,s,t)
+    e_out[a]=error_out(x,y,s,t)
 
+colors=np.random.rand(1000)
+plt.scatter(e_out,e_in,c=colors,alpha=0.3)
+plt.savefig('err.png')
+plt.show()
